a34f7f93f41a1a53e6f3031b48d474091b2e340b
dakota-peel <koteman624@gmail.com>
2015-12-09 16:49:00 -0800

    Added some Queue functionality

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,26 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function() {
-  }
 
+  initialize: function() {
+    // this.playFirst();
+    this.on('add', function () {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('ended', function () {
+      this.shift();
+      this.playFirst();
+    }, this);
+  },
+
+  defaults: {
+    
+  },
+
+  playFirst: function () {
+    this.at(0).play();
+  }
+  
 });
